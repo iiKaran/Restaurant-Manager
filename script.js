2d14fa83fabdbd918540180f716659eafe33ba92
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2023-02-03 02:30:23 +0530

    billing feature addded

--- script.js
+++ script.js
@@ -1,238 +1,248 @@
-
 let obj = {
-"pizzas": [
+  pizzas: [
     {
-      id: 1, 
-      name:" Cheese-pizza",
+      id: 1,
+      name: " Cheese-pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/cheese_pizza.jpg",
-      price:120,
-      oldPrice:180,
-      off:10
+      price: 120,
+      oldPrice: 180,
+      off: 10,
     },
     {
-      id:2 ,
+      id: 2,
       name: "Hungry Bytes",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/Hugry_bite_pizaa.jpg",
-      price:140,
-      oldPrice:160,
-      off:15
+      price: 140,
+      oldPrice: 160,
+      off: 15,
     },
     {
-      id:3 ,
+      id: 3,
       name: "Red Drop Pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/Red_drop_pizaa.jpg",
-      price:180,
-      oldPrice:240,
-      off:20
+      price: 180,
+      oldPrice: 240,
+      off: 20,
     },
-    { id :4 , 
+    {
+      id: 4,
       name: "Red Chilli Pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/Red_chilli_pizza.jpg",
-      price:190,
-      oldPrice:230,
-      off:10
+      price: 190,
+      oldPrice: 230,
+      off: 10,
     },
     {
-      id :5, 
+      id: 5,
       name: "Special Choco Pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/ChocoPizza.jpg",
-      price:320,
-      oldPrice:480,
-      off:14
+      price: 320,
+      oldPrice: 480,
+      off: 14,
     },
     {
-      id:6, 
+      id: 6,
       name: "Creamy Pizaa",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/creemypizza.jpg",
-      price:320,
-      oldPrice:430,
-      off:39
-    }
+      price: 320,
+      oldPrice: 430,
+      off: 39,
+    },
   ],
- "momos":
-  [{
-   id:7, 
-   name:"Mushroom Momos",
-   desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
-   src: "item_image/momo/mushroom_momo.jpg",
-   price:100,
-   oldPrice:130,
-   off:10
- },{
-  id:8,
-  name:"panner Momos",
-  desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
-  src: "item_image/momo/panner_momo.jpg",
-  price:170,
-  oldPrice:210,
-  off:10
-},{
- id:9,
- name:"Vegie Momos",
- desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
- src: "item_image/momo/veg_momo.jpg",
- price:110,
- oldPrice:120,
- off:10
-}
-],
-"burgers":[
- {
-  id:10,
-  name:"Chicken Burger",
-  desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
-  src: "item_image/Burger/Chicken_burger.jpg",
-  price:120,
-  oldPrice:180,
-  off:15
-},
-{
- id:11,
- name:"Egg Burger",
- desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
- src: "item_image/Burger/Egg_burger.jpg",
- price:90,
- oldPrice:120,
- off:35
-},
-{
- id:12,
- name:"Hungry Byte Burger",
- desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
- src: "item_image/Burger/hunger_bite_burger.jpg",
- price:190,
- oldPrice:120,
- off:15
-},
-{
- id :13,
- name:"Veg Cheese Burger",
- desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
- src: "item_image/Burger/veg_cheese_burger'.jpg",
- price:110,
- oldPrice:180,
- off:15
-},
-{
- id:14,
- name:"Veg Cheese Burger",
- desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
- src: "item_image/Burger/veg_cheese_burger'.jpg",
- price:110,
- oldPrice:180,
- off:15
-},
-{
- id:15,
- name:"Chicken Burger",
- desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
- src: "item_image/Burger/Chicken_burger.jpg",
- price:120,
- oldPrice:180,
- off:15
-}
-],
-"rolls":[ {
- id : 16, 
- name:"Spring rolls",
- desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
- src: "item_image/roll/spring_roll.jpg",
- price:120,
- oldPrice:180,
- off:15
-},
-{
- id :17,
- name:"Chicken roll",
- desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
- src: "item_image/roll/chicken_rool.jpg",
- price:120,
- oldPrice:180,
- off:15
-},{
- id:18,
- name:"Mallai roll",
- desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
- src: "item_image/roll/malaivegroll.jpg",
- price:120,
- oldPrice:180,
- off:15
-},{
- id:19,
- name:"Special sweet roll",
- desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
- src: "item_image/roll/sweet-roll.jpg",
- price:120,
- oldPrice:180,
- off:15
-},{
- id:20,
- name:"Veg Kathi roll",
- desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
- src: "item_image/roll/veg_kathi_roll.jpg",
- price:120,
- oldPrice:180,
- off:15
-},{
- id:21,
- name:"Veg roll",
- desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
- src: "item_image/roll/Veg_roll.jpg",
- price:120,
- oldPrice:180,
- off:15
-}
-],
-"chickens":[
- {
-  id:22,
-  name:"Chicken Kabab",
-  desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
-  src: "item_image/chicken/Chicken_Kabab.jpg",
-  price:120,
-  oldPrice:180,
-  off:15
- },
- {
-  id:23,
-  name:"Roasted Wings",
-  desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
-  src: "item_image/chicken/chicken_serve_single.jpg",
-  price:120,
-  oldPrice:180,
-  off:15
- },{
-  id:24,
-  name:"Fry Chicken",
-  desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
-  src: "item_image/chicken/fry_chicken.jpg",
-  price:120,
-  oldPrice:180,
-  off:15
- },{
-  id:25,
-  name:"Roasted Chicken ",
-  desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
-  src: "item_image/chicken/Roasted _chicken.jpg",
-  price:120,
-  oldPrice:180,
-  off:15
- },{
-  id:26,
-  name:"Stick Kbaab",
-  desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
-  src: "item_image/chicken/stick_kabab.jpg",
-  price:120,
-  oldPrice:180,
-  off:15
- }
-]
+  momos: [
+    {
+      id: 7,
+      name: "Mushroom Momos",
+      desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
+      src: "item_image/momo/mushroom_momo.jpg",
+      price: 100,
+      oldPrice: 130,
+      off: 10,
+    },
+    {
+      id: 8,
+      name: "panner Momos",
+      desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
+      src: "item_image/momo/panner_momo.jpg",
+      price: 170,
+      oldPrice: 210,
+      off: 10,
+    },
+    {
+      id: 9,
+      name: "Vegie Momos",
+      desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
+      src: "item_image/momo/veg_momo.jpg",
+      price: 110,
+      oldPrice: 120,
+      off: 10,
+    },
+  ],
+  burgers: [
+    {
+      id: 10,
+      name: "Chicken Burger",
+      desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
+      src: "item_image/Burger/Chicken_burger.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 11,
+      name: "Egg Burger",
+      desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
+      src: "item_image/Burger/Egg_burger.jpg",
+      price: 90,
+      oldPrice: 120,
+      off: 35,
+    },
+    {
+      id: 12,
+      name: "Hungry Byte Burger",
+      desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
+      src: "item_image/Burger/hunger_bite_burger.jpg",
+      price: 190,
+      oldPrice: 120,
+      off: 15,
+    },
+    {
+      id: 13,
+      name: "Veg Cheese Burger",
+      desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
+      src: "item_image/Burger/veg_cheese_burger'.jpg",
+      price: 110,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 14,
+      name: "Veg Cheese Burger",
+      desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
+      src: "item_image/Burger/veg_cheese_burger'.jpg",
+      price: 110,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 15,
+      name: "Chicken Burger",
+      desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
+      src: "item_image/Burger/Chicken_burger.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+  ],
+  rolls: [
+    {
+      id: 16,
+      name: "Spring rolls",
+      desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
+      src: "item_image/roll/spring_roll.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 17,
+      name: "Chicken roll",
+      desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
+      src: "item_image/roll/chicken_rool.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 18,
+      name: "Mallai roll",
+      desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
+      src: "item_image/roll/malaivegroll.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 19,
+      name: "Special sweet roll",
+      desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
+      src: "item_image/roll/sweet-roll.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 20,
+      name: "Veg Kathi roll",
+      desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
+      src: "item_image/roll/veg_kathi_roll.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 21,
+      name: "Veg roll",
+      desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
+      src: "item_image/roll/Veg_roll.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+  ],
+  chickens: [
+    {
+      id: 22,
+      name: "Chicken Kabab",
+      desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
+      src: "item_image/chicken/Chicken_Kabab.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 23,
+      name: "Roasted Wings",
+      desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
+      src: "item_image/chicken/chicken_serve_single.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 24,
+      name: "Fry Chicken",
+      desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
+      src: "item_image/chicken/fry_chicken.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 25,
+      name: "Roasted Chicken ",
+      desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
+      src: "item_image/chicken/Roasted _chicken.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+    {
+      id: 26,
+      name: "Stick Kbaab",
+      desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
+      src: "item_image/chicken/stick_kabab.jpg",
+      price: 120,
+      oldPrice: 180,
+      off: 15,
+    },
+  ],
 };
 populate("pizzas");
 pizza_item = document.getElementById("pizzas");
@@ -256,7 +266,8 @@ function populate(id) {
   // console.log(obj[id]);
   arr = obj[id];
   Array.from(arr).forEach((e) => {
-    row.innerHTML += `<div class="card" data-aos="fade-in" data-aos-anchor=".other-element" data-aos-duration="4000">
+   // data-aos="fade-in" data-aos-anchor=".other-element" data-aos-duration="3000"
+    row.innerHTML += `<div class="card" >
   <h2>${e.name}</h2>
   <img src="${e.src}" alt="">
   <p>${e.desc}</p>
@@ -273,43 +284,39 @@ function removeFromAll() {
     e.classList.remove("active");
   });
 }
-function addToCart(element)
-{
- cart = document.getElementById("cart"); 
- cart.style.left = "60%";
- table = document.getElementById("table");
- price = document.getElementById("amt");
- let sum =0 ; 
- table.innerHTML += `  <div class="table-row">
+function addToCart(element) {
+  cart = document.getElementById("cart");
+  cart.style.left = "60%";
+  table = document.getElementById("table");
+  price = document.getElementById("amt");
+  let sum = 0;
+  table.innerHTML += `<div class="table-row flex">
  <img src=${element.src} alt="">
  <div class="item-name">${element.name}</div>
- <div class="item-quantity"><input type="number" name="" id="qty" value="1"></div>
+ <div class="item-quantity"><input type="number" name="" id="qty" value="1" onchange="updatePrice()"></div>
  <div class="price">${element.price}-/-</div>
-</div>`
+</div>`;
+  price.innerText = `${parseInt(price.innerText) + element.price}`;
 }
-cat = ["pizzas","rolls", "momos","burgers","chickens" ]
-function cartPopulate(id)
-{
- // console.log(obj)
- // console.log(id)
- for(let i=0 ; i< cat.length ;i++)
- {
-  for(let j =0 ; j < obj[cat[i]].length;j++)
-  {
-   if(obj[cat[i]][j].id===parseInt(id))
-   {
-    addToCart(obj[cat[i]][j]);
-   }
+cat = ["pizzas", "rolls", "momos", "burgers", "chickens"];
+function cartPopulate(id) {
+  // console.log(obj)
+  // console.log(id)
+  for (let i = 0; i < cat.length; i++) {
+    for (let j = 0; j < obj[cat[i]].length; j++) {
+      if (obj[cat[i]][j].id === parseInt(id)) {
+        addToCart(obj[cat[i]][j]);
+      }
+    }
   }
- }
 }
 
-function addmore()
-{
-cart.style.left="100%";
+function addmore() {
+  cart.style.left = "100%";
 }
-function clr()
-{
-table.innerHTML='';
-addmore();
+function clr() {
+  table.innerHTML = "";
+  price = document.getElementById("amt");
+  price.innerText = "0";
+  addmore();
 }
