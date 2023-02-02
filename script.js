d9bf3cf95eb21a72cdbdd1c0224f12aec5c686f7
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2023-02-03 00:43:41 +0530

    Cart Added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let obj = {
 "pizzas": [
     {
+      id: 1, 
       name:" Cheese-pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/cheese_pizza.jpg",
@@ -10,6 +11,7 @@ let obj = {
       off:10
     },
     {
+      id:2 ,
       name: "Hungry Bytes",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/Hugry_bite_pizaa.jpg",
@@ -18,6 +20,7 @@ let obj = {
       off:15
     },
     {
+      id:3 ,
       name: "Red Drop Pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/Red_drop_pizaa.jpg",
@@ -25,7 +28,7 @@ let obj = {
       oldPrice:240,
       off:20
     },
-    {
+    { id :4 , 
       name: "Red Chilli Pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/Red_chilli_pizza.jpg",
@@ -34,6 +37,7 @@ let obj = {
       off:10
     },
     {
+      id :5, 
       name: "Special Choco Pizza",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/ChocoPizza.jpg",
@@ -42,63 +46,18 @@ let obj = {
       off:14
     },
     {
+      id:6, 
       name: "Creamy Pizaa",
       desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
       src: "item_image/pizaa/creemypizza.jpg",
       price:320,
       oldPrice:430,
       off:39
-    },{
-     name:" Cheese-pizza",
-     desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
-     src: "item_image/pizaa/cheese_pizza.jpg",
-     price:120,
-     oldPrice:180,
-     off:10
-   },
-   {
-     name: "Hungry Bytes",
-     desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
-     src: "item_image/pizaa/Hugry_bite_pizaa.jpg",
-     price:140,
-     oldPrice:160,
-     off:15
-   },
-   {
-     name: "Red Drop Pizza",
-     desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
-     src: "item_image/pizaa/Red_drop_pizaa.jpg",
-     price:180,
-     oldPrice:240,
-     off:20
-   },
-   {
-     name: "Red Chilli Pizza",
-     desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
-     src: "item_image/pizaa/Red_chilli_pizza.jpg",
-     price:190,
-     oldPrice:230,
-     off:10
-   },
-   {
-     name: "Special Choco Pizza",
-     desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
-     src: "item_image/pizaa/ChocoPizza.jpg",
-     price:320,
-     oldPrice:480,
-     off:14
-   },
-   {
-     name: "Creamy Pizaa",
-     desc: "Make your night and sleep chessy with the cheese pizza at best price with best staff and best quality",
-     src: "item_image/pizaa/creemypizza.jpg",
-     price:320,
-     oldPrice:430,
-     off:39
-   }
+    }
   ],
  "momos":
   [{
+   id:7, 
    name:"Mushroom Momos",
    desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
    src: "item_image/momo/mushroom_momo.jpg",
@@ -106,6 +65,7 @@ let obj = {
    oldPrice:130,
    off:10
  },{
+  id:8,
   name:"panner Momos",
   desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
   src: "item_image/momo/panner_momo.jpg",
@@ -113,6 +73,7 @@ let obj = {
   oldPrice:210,
   off:10
 },{
+ id:9,
  name:"Vegie Momos",
  desc: "Make your night and sleep chessy with the Momos at best price with best staff and best quality",
  src: "item_image/momo/veg_momo.jpg",
@@ -123,6 +84,7 @@ let obj = {
 ],
 "burgers":[
  {
+  id:10,
   name:"Chicken Burger",
   desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
   src: "item_image/Burger/Chicken_burger.jpg",
@@ -131,6 +93,7 @@ let obj = {
   off:15
 },
 {
+ id:11,
  name:"Egg Burger",
  desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
  src: "item_image/Burger/Egg_burger.jpg",
@@ -139,6 +102,7 @@ let obj = {
  off:35
 },
 {
+ id:12,
  name:"Hungry Byte Burger",
  desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
  src: "item_image/Burger/hunger_bite_burger.jpg",
@@ -147,6 +111,7 @@ let obj = {
  off:15
 },
 {
+ id :13,
  name:"Veg Cheese Burger",
  desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
  src: "item_image/Burger/veg_cheese_burger'.jpg",
@@ -155,6 +120,7 @@ let obj = {
  off:15
 },
 {
+ id:14,
  name:"Veg Cheese Burger",
  desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
  src: "item_image/Burger/veg_cheese_burger'.jpg",
@@ -163,6 +129,7 @@ let obj = {
  off:15
 },
 {
+ id:15,
  name:"Chicken Burger",
  desc: "Make your night and sleep chessy with the Burgers at best price with best staff and best quality",
  src: "item_image/Burger/Chicken_burger.jpg",
@@ -172,6 +139,7 @@ let obj = {
 }
 ],
 "rolls":[ {
+ id : 16, 
  name:"Spring rolls",
  desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
  src: "item_image/roll/spring_roll.jpg",
@@ -180,6 +148,7 @@ let obj = {
  off:15
 },
 {
+ id :17,
  name:"Chicken roll",
  desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
  src: "item_image/roll/chicken_rool.jpg",
@@ -187,6 +156,7 @@ let obj = {
  oldPrice:180,
  off:15
 },{
+ id:18,
  name:"Mallai roll",
  desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
  src: "item_image/roll/malaivegroll.jpg",
@@ -194,6 +164,7 @@ let obj = {
  oldPrice:180,
  off:15
 },{
+ id:19,
  name:"Special sweet roll",
  desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
  src: "item_image/roll/sweet-roll.jpg",
@@ -201,6 +172,7 @@ let obj = {
  oldPrice:180,
  off:15
 },{
+ id:20,
  name:"Veg Kathi roll",
  desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
  src: "item_image/roll/veg_kathi_roll.jpg",
@@ -208,6 +180,7 @@ let obj = {
  oldPrice:180,
  off:15
 },{
+ id:21,
  name:"Veg roll",
  desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
  src: "item_image/roll/Veg_roll.jpg",
@@ -218,6 +191,7 @@ let obj = {
 ],
 "chickens":[
  {
+  id:22,
   name:"Chicken Kabab",
   desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
   src: "item_image/chicken/Chicken_Kabab.jpg",
@@ -226,6 +200,7 @@ let obj = {
   off:15
  },
  {
+  id:23,
   name:"Roasted Wings",
   desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
   src: "item_image/chicken/chicken_serve_single.jpg",
@@ -233,6 +208,7 @@ let obj = {
   oldPrice:180,
   off:15
  },{
+  id:24,
   name:"Fry Chicken",
   desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
   src: "item_image/chicken/fry_chicken.jpg",
@@ -240,6 +216,7 @@ let obj = {
   oldPrice:180,
   off:15
  },{
+  id:25,
   name:"Roasted Chicken ",
   desc: "Make your night and sleep chessy with the Chicken at best price with best staff and best quality",
   src: "item_image/chicken/Roasted _chicken.jpg",
@@ -247,6 +224,7 @@ let obj = {
   oldPrice:180,
   off:15
  },{
+  id:26,
   name:"Stick Kbaab",
   desc: "Make your night and sleep chessy with the Rolls at best price with best staff and best quality",
   src: "item_image/chicken/stick_kabab.jpg",
@@ -257,7 +235,6 @@ let obj = {
 ]
 };
 populate("pizzas");
-console.log("hello ji");
 pizza_item = document.getElementById("pizzas");
 burger_item = document.getElementById("burgers");
 chicken_item = document.getElementById("chickens");
@@ -276,12 +253,10 @@ Array.from(item).forEach((e) => {
   });
 });
 function populate(id) {
-  console.log(id);
   // console.log(obj[id]);
   arr = obj[id];
   Array.from(arr).forEach((e) => {
-    console.log(e);
-    row.innerHTML += `<div class="card" data-aos="fade-in" data-aos-anchor=".other-element" data-aos-duration="3000">
+    row.innerHTML += `<div class="card" data-aos="fade-in" data-aos-anchor=".other-element" data-aos-duration="4000">
   <h2>${e.name}</h2>
   <img src="${e.src}" alt="">
   <p>${e.desc}</p>
@@ -289,7 +264,7 @@ function populate(id) {
     <div class="price">Rs.${e.price} <span>${e.oldPrice}</span></div>
     <div class="offer">${e.off}% off</div>
   </div>
-  <div class="add-to-cart">Add to Cart</div>
+  <div class="add-to-cart" id="${e.id}" onClick ="cartPopulate(this.id)">Add to Cart</div>
 </div>`;
   });
 }
@@ -297,4 +272,44 @@ function removeFromAll() {
   Array.from(item).forEach((e) => {
     e.classList.remove("active");
   });
+}
+function addToCart(element)
+{
+ cart = document.getElementById("cart"); 
+ cart.style.left = "60%";
+ table = document.getElementById("table");
+ price = document.getElementById("amt");
+ let sum =0 ; 
+ table.innerHTML += `  <div class="table-row">
+ <img src=${element.src} alt="">
+ <div class="item-name">${element.name}</div>
+ <div class="item-quantity"><input type="number" name="" id="qty" value="1"></div>
+ <div class="price">${element.price}-/-</div>
+</div>`
+}
+cat = ["pizzas","rolls", "momos","burgers","chickens" ]
+function cartPopulate(id)
+{
+ // console.log(obj)
+ // console.log(id)
+ for(let i=0 ; i< cat.length ;i++)
+ {
+  for(let j =0 ; j < obj[cat[i]].length;j++)
+  {
+   if(obj[cat[i]][j].id===parseInt(id))
+   {
+    addToCart(obj[cat[i]][j]);
+   }
+  }
+ }
+}
+
+function addmore()
+{
+cart.style.left="100%";
+}
+function clr()
+{
+table.innerHTML='';
+addmore();
 }
